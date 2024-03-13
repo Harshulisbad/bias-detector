0e937aa15688fae4322d0ba02c97a7288972f9cf
Harshulisbad <93846109+Harshulisbad@users.noreply.github.com>
2024-03-13 00:11:48 -0700

    Update website.py
    tessttt

--- website.py
+++ website.py
@@ -25,6 +25,8 @@ def classification(text, out):
   PalestineM=0
   IsraelM=0
   classified = query({"inputs": text, "options": {"wait_for_model": True}})
+  print(classified)
+  print(type(classified))
   scores = [0]*12
   
   for label in classified[0]:
